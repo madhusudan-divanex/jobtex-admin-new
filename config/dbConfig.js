939e8112722c17dde5cb87fa9e08de9b3fe2ddf6
madhusudan-divanex <madhusudhandivanextechnologies@gmail.com>
2025-04-05 10:38:23 +0530

    db changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 async function ConnectToDb(){
     try {
        // mongodb+srv://madhusudan:madhusudan@123@jobtex.5qbet8z.mongodb.net/?retryWrites=true&w=majority&appName=jobtex
-        await mongoose.connect('mongodb+srv://madhusudan:madhusudan123@jobtex.5qbet8z.mongodb.net/jobtex')
+        await mongoose.connect('mongodb+srv://madhusudan:madhusudan123@jobtex.5qbet8z.mongodb.net/jobtex', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
        
         console.log("connected successfully ✅")
     } catch (error) {
