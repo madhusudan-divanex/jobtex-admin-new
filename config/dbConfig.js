934b9850051680b675ae861bd996dc57e3f4bc7d
madhusudan-divanex <madhusudhandivanextechnologies@gmail.com>
2025-04-05 12:31:17 +0530

    re

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 async function ConnectToDb(){
     try {
        // mongodb+srv://madhusudan:madhusudan@123@jobtex.5qbet8z.mongodb.net/?retryWrites=true&w=majority&appName=jobtex
-        await mongoose.connect('mongodb+srv://madhusudan:madhusudan123@jobtex.5qbet8z.mongodb.net/jobtex',{ serverSelectionTimeoutMS: 10000,  // 10 seconds timeout for connecting
-            socketTimeoutMS: 45000,           // Optional: How long to keep socket open
+        await mongoose.connect('mongodb+srv://madhusudan:madhusudan123@jobtex.5qbet8z.mongodb.net/jobtex',{ serverSelectionTimeoutMS: 100000,  // 10 seconds timeout for connecting
+            socketTimeoutMS: 450000,           // Optional: How long to keep socket open
             bufferCommands: true,             // Enable command buffering
-            bufferTimeoutMS: 10000,    });
+            bufferTimeoutMS: 100000,    });
        
         console.log("connected successfully ✅")
     } catch (error) {
