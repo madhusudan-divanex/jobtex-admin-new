4f19341ce460d2311faaeb0d7054c15e27b0065c
madhusudan-divanex <madhusudhandivanextechnologies@gmail.com>
2025-04-05 12:11:28 +0530

    increase socket time

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 async function ConnectToDb(){
     try {
        // mongodb+srv://madhusudan:madhusudan@123@jobtex.5qbet8z.mongodb.net/?retryWrites=true&w=majority&appName=jobtex
-        await mongoose.connect('mongodb+srv://madhusudan:madhusudan123@jobtex.5qbet8z.mongodb.net/jobtex');
+        await mongoose.connect('mongodb+srv://madhusudan:madhusudan123@jobtex.5qbet8z.mongodb.net/jobtex', {
+            serverSelectionTimeoutMS: 5000, // Fail fast if MongoDB is unreachable
+            socketTimeoutMS: 45000, // Keep sockets open for a long query
+        });
        
         console.log("connected successfully ✅")
     } catch (error) {
