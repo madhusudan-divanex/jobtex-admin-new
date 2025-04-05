db57080fe7bbf4e3be158a804adee67fd9f99a61
madhusudan-divanex <madhusudhandivanextechnologies@gmail.com>
2025-04-05 13:28:09 +0530

    node modules delted

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 async function ConnectToDb(){
     try {
        // mongodb+srv://madhusudan:madhusudan@123@jobtex.5qbet8z.mongodb.net/?retryWrites=true&w=majority&appName=jobtex
-        await mongoose.connect('mongodb+srv://madhusudan:madhusudan123@jobtex.5qbet8z.mongodb.net/jobtex');
+        await mongoose.connect(process.env.MONGO_URL);
        
         console.log("connected successfully ✅")
     } catch (error) {
