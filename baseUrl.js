1d32aa1a1abacfb82ad40b7c074fa58b20ed285a
madhusudan-divanex <madhusudhandivanextechnologies@gmail.com>
2025-04-10 18:20:20 +0530

    done

--- baseUrl.js
+++ baseUrl.js
@@ -1,7 +1,7 @@
-export const base_url = 'https://tailored-job.vercel.app/'
+export const base_url = 'https://tailored-job.vercel.app'
 
 // https://jobtex.divanex.in'
 
 // http://localhost:7000
 
-// https://tailored-job.vercel.app/
+// https://tailored-job.vercel.app
